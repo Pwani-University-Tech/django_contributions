9c4e0136c294621cdc9b8a2c7199a4d000be4afe
Lewis <lewisomogi01@gmail.com>
2025-03-14 00:03:47 +0300

    deployed views and endpoints

--- ToDoListAPI/settings.py
+++ ToDoListAPI/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'to_do_app',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
--- to_do_app/views.py
+++ to_do_app/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for viewing and editing tasks.
+    """
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['completed', 'due_date']
+    search_fields = ['title', 'description']
+    ordering_fields = ['created_at', 'updated_at', 'due_date']
+    ordering = ['-created_at']
+
+    def get_queryset(self):
+        """
+        This view should return a list of all tasks
+        for the currently authenticated user.
+        """
+        return Task.objects.filter(user=self.request.user)
+
+    @action(detail=False, methods=['get'])
+    def completed_tasks(self):
+        """
+        Return all completed tasks for the current user.
+        """
+        tasks = self.get_queryset().filter(completed=True)
+        serializer = self.get_serializer(tasks, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'])
+    def pending_tasks(self):
+        """
+        Return all pending tasks for the current user.
+        """
+        tasks = self.get_queryset().filter(completed=False)
+        serializer = self.get_serializer(tasks, many=True)
+        return Response(serializer.data)
